04ed6f8fca3904bb0bc12edc4711334ef5ebcf81
Takahiro Ushijima <v19e3004@oita-u.ac.jp>
2020-05-24 08:53:15 +0000

    Lesson 03課題

---
+++ hello.rb
@@ -0,0 +1,27 @@
+# Lesson2
+#コメント
+=begin
+複数行コメント
+複数行コメント
+=end
+=begin
+print "hello world"
+puts "hello world" # +改行
+p "hello world" # デバッグ用
+=end
+
+# Lesson3
+#変数
+msg = "hello world"
+puts msg
+
+msg = "hello world again"
+puts msg
+
+#定数
+
+VERSION = 1.1
+puts VERSION
+
+VERSION = 1.2
+puts VERSION
